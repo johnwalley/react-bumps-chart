99a5e2ea15e4267733fc01812f0841ee902997f8
johnwalley <john@walley.org.uk>
2020-08-08 18:13:00 +0100

    chore: remove unused key

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-  webpackFinal: async config => {
-    // do mutation to the config
-
-    return config;
-  },
 };
