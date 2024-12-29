faa11e935d20ee60a90c0fa5819d994ebd32d237
johnwalley <john@walley.org.uk>
2024-12-29 21:32:19 +0000

    fix: improve react 19 support

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,15 +2,24 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-  docs: {},
+
+  docs: {
+    autodocs: true
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,8 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react/jsx-runtime",
+      ],
     },
   },
   css: {
